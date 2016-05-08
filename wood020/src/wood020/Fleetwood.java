60c244f89b26caa1c1bbcd8c9bf36e149792cad8
ANUJI <anuji.de.silva@gmail.com>
2016-05-08 14:07:52 +0530

    commite initilizal project source code

---
+++ wood020/src/wood020/Fleetwood.java
@@ -0,0 +1,14 @@
+package wood020;
+public class Fleetwood {
+  static transient String[] chameleon = {
+    "I never believed there was one code of morality for a public and another for a private man.","Thomas Jefferson","",
+    "A man never lies as much as after a hunt, during a war, and before an election.","Otto von Bismarck","",
+    "The man who views the world at 50 the same as he did at 20 has wasted 30 years of his life.","Muhammad Ali","",
+    "Truth is generally the best vindication against slander.","Abraham Lincoln","",
+    "Wickedness is a myth invented by good people to account for the curious attraction of others.","Oscar Wilde","",
+    "Great liars are also great magicians.","Adolf Hitler","",
+    "Fallacies do not cease to be fallacies because they become fashions.","GK Chesterton","",
+    "Freedom is hammered out on the anvil of discussion, dissent, and debate.","Hubert H. Humphrey","",
+    "Experience is not what happens to a man; it is what a man does with what happens to him.","Aldous Huxley","",
+    "Truth is confirmed by inspection and delay; falsehood by haste and uncertainty.","Tacitus",""};
+}
---
+++ wood020/src/wood020/IOSpecialist.java
@@ -0,0 +1,14 @@
+package wood020;
+
+/**
+ * @author Kevan Buckley, maintained by Jamie Wood
+ * @version 2.0, 2014
+ */
+
+public class IOSpecialist {
+  public IOSpecialist() {   
+  }
+  public String getString(){
+    return IOLibrary.getString();
+  }
+}
